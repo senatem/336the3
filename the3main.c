f534c73559b2c16149c23119c5330abd4e30fff8
Sena Temuçin <sena.temucin@metu.edu.tr>
2018-05-01 17:42:48 +0300

    ADC configuration changed.

--- the3main.c
+++ the3main.c
@@ -8,6 +8,7 @@
 #include <p18cxxx.h>
 #include <p18f8722.h>
 #include <xc.h>
+#include <string.h>
 #pragma config OSC = HSPLL, FCMEN = OFF, IESO = OFF, PWRT = OFF, BOREN = OFF, WDT = OFF, MCLRE = ON, LPT1OSC = OFF, LVP = OFF, XINST = OFF, DEBUG = OFF
 
 #define _XTAL_FREQ   40000000
@@ -23,8 +24,8 @@ int tmr1helper = 0;
 int currpin=0;
 int blank=0;
 int digit=0;
-char number=' ';
-char *pin='';
+char number='#';
+char *pin;
 
 void Init(){
     PORTE = 0;
@@ -37,8 +38,9 @@ void Init(){
     PIR1 = 0;
     PIE1 = 1;
     IPR1 = 0;
-    ADCON0 = 1;
+    ADCON0 = 0x31;
     ADCON1 = 0;
+    ADCON2 = 0x8A;
     return;
 }
 
@@ -47,13 +49,11 @@ void interrupt mainISR(){
         TMR0IF=0;
         TMR0L = 0x3D;
         tmr0helper+=1;
-        if (tmr0helper==20&&state==1){
+        if ((tmr0helper%20)==0&&state==1){
             /*ADC code*/
-            ADCON0bits.GODONE = 1;
-            tmr0helper=0;
+            ADCON0bits.GO = 1;
         }
-        if (tmr0helper==50&&state==1){
-            tmr0helper=0;
+        if ((tmr0helper%50==0)&&state==1){
             WriteCommandToLCD(0x8B+currpin);
             if(blank){
                 WriteDataToLCD(number);
@@ -84,13 +84,42 @@ void interrupt mainISR(){
         RBIF=0;
         /*rb6&rb7 isr code*/
     }
-    else if(ADIF){
+    else if(PIR1bits.ADIF){
         ADIF=0;
         /*adcon code*/
-        digit = (ADRESH<<4)+ADRESL;
-        digit = (digit-99)/100;
-        number = digit+65;
-        pin = pin.strcon(number);
+        digit = ADRESL|(ADRESH<<8);
+        digit = (digit-99);
+        if(digit<100){
+            digit=0;
+        }
+        else if(99<digit<200){
+            digit=1;
+        }
+        else if(199<digit<300){
+            digit=2;
+        }
+        else if(299<digit<400){
+            digit=3;
+        }
+        else if(399<digit<500){
+            digit=4;
+        }
+        else if(499<digit<600){
+            digit=5;
+        }
+        else if(599<digit<700){
+            digit=6;
+        }
+        else if(699<digit<800){
+            digit=7;
+        }
+        else if(799<digit<900){
+            digit=8;
+        }
+        else if(899<digit<1024){
+            digit=9;
+        }
+        debug=1;
     }
 }
 
@@ -132,7 +161,12 @@ int main(int argc, char** argv) {
     TMR1 = 0x0BDC;
     T0CON = 0xC7;
     T1CON = 0xF9;
+    ADIF = 0;
+    ADIE = 1;
     ei();
+    while(1){
+        
+    }
     
 }
 
