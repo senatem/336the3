b148b2218a6b151f0a3a1a53420b84b9b59e28bd
Sena Temuçin <sena.temucin@metu.edu.tr>
2018-05-02 13:55:53 +0300

    Done, except for a tiny bug

--- the3main.c
+++ the3main.c
@@ -1,6 +1,6 @@
 /* 
  * File:   the3main.c
- * Author: Sena Temuçin
+ * Author: Sena Temucin
  *
  * Created on April 22, 2018, 5:29 PM
  */
@@ -34,12 +34,13 @@ int blank=0;
 int digit=0;
 char number='#';
 int pin[4];
-int msgblink=0;
-int a,b,c,d,e,f,g,h,timeleft=120;
+int test=0,att=2;
+int msgblink=0,penalty=0;
+int a=0,b,c=2,d,e=1,f,g=0,h,timeleft=120;
 
 
 
-void Init(){
+void Init(){        //Initialize SFR values, timers, and interrupts
     TRISA = 0xFF;
     TRISF = 0xFF;
     PORTE = 0;
@@ -56,18 +57,18 @@ void Init(){
     ADCON1 = 0;
     ADCON2 = 0x1A;
     return;
-}
+}  
 
-void interrupt mainISR(){
-    if(TMR0IF){
-        TMR0IF=0;
+void interrupt mainISR(){       
+    if(TMR0IF){         //Timer0 ISR checks the states and performs necessary operations
+        TMR0IF=0;       //based on current state
         TMR0L = 0x3D;
         tmr0helper+=1;
-        if ((tmr0helper%20)==0&&state==1){
+        if (((tmr0helper%20)==0)&&((state==1)||(state==3))){    //ADC conversion request once every 100 ms
             /*ADC code*/
             ADCON0bits.GO = 1;
         }
-        if ((tmr0helper%50==0)&&state==1){
+        if (((tmr0helper%50)==0)&&((state==1)||(state==3))){    //Pin entry blinking once every 250 ms
             if(currpin<7){
                 WriteCommandToLCD(0x8B+currpin/2);
             }
@@ -80,7 +81,7 @@ void interrupt mainISR(){
                 blank=1;
             }
         }
-        if((tmr0helper%100==0)&&(state==2)){
+        if((tmr0helper%100==0)&&(state==2)){        //Pin set complete, 3 seconds of blinking
             if(blank){
                 WriteStringToLCD(" The new pin is ");
                 WriteCommandToLCD(0xC0);  
@@ -91,40 +92,80 @@ void interrupt mainISR(){
                 WriteDataToLCD(pin[3]+48);
                 WriteStringToLCD("---   ");
                 blank=0;
+                currpin=0;
                 }
             else{
                 ClearLCDScreen();
                 blank=1;}
             msgblink+=1;
-            if(msgblink==6){state=3;}
+            if(msgblink==6){        //Change the message and the state after 3 seconds
+                ClearLCDScreen();
+                WriteCommandToLCD(0x80);
+                WriteStringToLCD(" Enter pin:#### ");
+                WriteCommandToLCD(0xC0);                
+                WriteStringToLCD("  Attempts:");
+                WriteDataToLCD(att+48);
+                
+                state=3;
+            }
             tmr0helper=0;
             }
     }
-    if(TMR1IF){
+    if(TMR1IF){     //7-segment operations
         TMR1IF=0;
-        TMR1 = 0x0BDC;
-        if(state<3){
+        TMR1 = 0x9E58;
+        if(state<3){        //Display "----"
             PORTJ=64;
             tl1=1;
             tl2=1;
             tl3=1;
             tl4=1;
         }
-        else if(state==3){
+        else if(state>2){       //Countdown from 120
             tmr1helper+=1;
             PORTJ=lookup[g];
             tl1=1;
+            __delay_ms(3);
+            tl1=0;
             
             PORTJ=lookup[e];
             tl2=1;
+            __delay_ms(3);
+            tl2=0;
 
             PORTJ=lookup[c];
             tl3=1;
+            __delay_ms(3);
+            tl3=0;
 
             PORTJ=lookup[a];
             tl4=1;
-        
-            if(tmr1helper==20){
+            __delay_ms(3);
+            tl4=0;
+            if((tmr1helper==50)&&(state==5)){       //20 seconds wait
+                penalty+=1;
+                currpin=0;}
+            if(penalty==20){        //Continue after 20 sec
+                state=3; 
+                penalty=0;
+                att=2;
+                ClearLCDScreen();
+                WriteCommandToLCD(0x80);
+                WriteStringToLCD(" Enter pin:#### ");
+                WriteCommandToLCD(0xC0);                
+                WriteStringToLCD("  Attempts:");
+                WriteDataToLCD(att+48);
+                }
+            if((tmr1helper==50)&&(state!=4)){       //7-segment display 120 secs counter code
+                if(!timeleft){      //Reset program after 120 seconds
+                    msgblink=0;
+                    currpin=0;
+                    state=1;
+                    timeleft=120;
+                    ClearLCDScreen();
+                    WriteCommandToLCD(0x80);
+                    WriteStringToLCD(" Set a pin:#### ");
+                }
                 timeleft-=1;
                 tmr1helper=0;
                 /*7-segment update code*/
@@ -136,26 +177,56 @@ void interrupt mainISR(){
                 f=d/10;
                 g=f%10; //1st digit is saved here
                 h=f/10;
-
-                
             }
         }
     }
-    if(INTCONbits.RBIF){
+    if(INTCONbits.RBIF){        //RB Port change interrupt ISR
         RBIF=0;
         /*rb6&rb7 isr code*/
-        if(!PORTBbits.RB6){
+        if((!PORTBbits.RB6)&&(state==1)){       //Set digit
             WriteCommandToLCD(0x8B+currpin/2);
             WriteDataToLCD(digit+48);
             pin[currpin/2] = digit;
-            if(currpin!=7){
+            if(currpin<7){
                 currpin+=1;}
         }
-        if(!PORTBbits.RB7&&(currpin>6)){
+        else if((!PORTBbits.RB6)&&(state==3)){      //Test current digit
+            WriteCommandToLCD(0x8B+currpin/2);
+            WriteDataToLCD(digit+48);
+            if((pin[currpin/2])==digit){test+=1;}
+            if(currpin<7){
+                currpin+=1;}
+        }
+        if((!PORTBbits.RB7)&&(currpin>6)&&(state==1)){      //Finish pin setting
             state=2;
+            currpin=0;
+        }
+        else if((!PORTBbits.RB7)&&(currpin>6)&&(state==3)){     //Test pin
+            if(test>7){
+                ClearLCDScreen();
+                WriteCommandToLCD(0x80);
+                WriteStringToLCD("Safe is opening!");
+                WriteCommandToLCD(0xC0);                
+                WriteStringToLCD("$$$$$$$$$$$$$$$$");
+                state=4;
+                currpin=0;
+            }
+            else{       //Decrease remaining attempt count
+                att=att-1;
+                WriteCommandToLCD(0xCB);
+                WriteDataToLCD((att)+48);
+                if(!att){
+                    WriteCommandToLCD(0x8B);
+                    WriteStringToLCD("XXXX");
+                    WriteCommandToLCD(0xC0);
+                    WriteStringToLCD("Try after 20 sec");
+                    state=5;
+                }
+                currpin=0;
+            }
         }
     }
-    if(PIR1bits.ADIF){
+    if(PIR1bits.ADIF){      //AD Converter ISR
         ADIF=0;
         /*adcon code*/
         digit = ADRESL|(ADRESH<<8);
@@ -174,7 +245,7 @@ void interrupt mainISR(){
 }
 
 
-void buttonE(){
+void buttonE(){     //RE1 button task
     if(PORTEbits.RE1==0 && buttonSet==1){
         for(int i=0;i<30091;i++){
             for(int j=0;j<81;j++){}
@@ -200,24 +271,21 @@ int main(int argc, char** argv) {
     WriteCommandToLCD(0xC0);  
     WriteStringToLCD(" $$$$$$$$$$$$$$ ");
     
-    while(!state){
+    while(!state){      //Wait until RE1 is pushed
         buttonE();
     }
     
-    ClearLCDScreen();
+    ClearLCDScreen();       
     WriteCommandToLCD(0x80);
-    WriteStringToLCD(" Set a pin:#### ");
+    WriteStringToLCD(" Set a pin:#### ");       //Start pin setting phase
     TMR0L = 0x3D;
-    TMR1L = 0xDC;
-    TMR1H = 0x0B;
+    TMR1 = 0x9E58;
     T0CON = 0xC7;
     T1CON = 0xF9;
     ADIF = 0;
     ADIE = 1;
     RBIE = 1;
-    ei();
+    ei();       //Enable interrupts
     while(1){
     }
-    
 }
-
